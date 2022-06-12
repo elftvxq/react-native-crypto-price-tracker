aaeea831876aa1e317a8dbf601c32e8ccc605d3b
Emma Shih <emma.shih@shopee.com>
2022-06-12 14:18:55 +0800

    reconstructure the coin detail fold and extract coindetail page header

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { Ionicons, EvilIcons } from '@expo/vector-icons';
+import Coin from '../../../assets/data/crypto.json';
+import CoinDetailHeader from './components/CoinDetailHeader';
 
 const CoinDetailedScreen = () => {
+  const {
+    image: { small },
+    symbol,
+    name,
+    market_data: { market_cap_rank },
+  } = Coin;
   return (
     <View>
-      <Text style={{ color: 'white' }}>Detail </Text>
+      <CoinDetailHeader
+        image={small}
+        name={name}
+        symbol={symbol}
+        marketCapRank={market_cap_rank}
+      />
     </View>
   );
 };
