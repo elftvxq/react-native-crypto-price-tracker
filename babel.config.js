1c0a3f804ec4648c009b2242096ce2551f867d80
Emma Shih <emma.shih@shopee.com>
2022-06-12 15:37:48 +0800

    install react native chart library and add babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // this plugin has to be the last one
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
