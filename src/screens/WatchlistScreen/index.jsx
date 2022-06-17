fceb615e14f8299202be4333003c0e747763818f
Emma Shih <emma.shih@shopee.com>
2022-06-18 00:11:08 +0800

    add watchlist tab and dummy page

---
+++ src/screens/WatchlistScreen/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WatchlistScreen = () => {
+  return (
+    <View>
+      <Text style={{ color: 'white' }}>Watch List</Text>
+    </View>
+  );
+};
+
+export default WatchlistScreen;
